6f6952e6b22b7028e524a22770b7b1dbdff33306
Muhammad-Bilal1605 <zaheermbilal1605@gmail.com>
2026-04-09 12:59:57 +0500

    error fixing

--- scripts/chatbot_variants/c_with_resume_chat/frontend.py
+++ scripts/chatbot_variants/c_with_resume_chat/frontend.py
@@ -1,14 +1,18 @@
+import sys
+import os
+
+# Add project root to path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))) # Needed just because of python ide error not like recognizing scripts folder
+
 import streamlit as st
 from scripts.chatbot_variants.c_with_resume_chat.backend import chatbot
 from langchain_core.messages import HumanMessage, AIMessageChunk
-# wo he wala hum nh config file set ki ha as we do for configuration for persistent memory and sth aik thread id attach kr di ha
+
+
 CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
-# st.session_data aik streamlit k andr library ha jo basically hamain allow krti ha k data aik session ka hum save kr skein
-#what we are doing here is k like ab ap aik message bhejogy phr dusra message and so on tu hr baar apki ye wali script shuru sh chlti ha
-#and session data store krnay ka b faida na hota as initialize shuru mai kr rhy thy message history ko tu wo empty ho jati
-#,,,jis ki wjh sh ap ka pichlay walay messages khtm ho jatay tu hum kia karaingy k k agr chat_messages mai koi message nhi tu hum
-#us session k liye aik dict bna laingy ,, is trh srf pehl dfa apki messages wali array initialize hogi, allowing you to keep previous msgs
+
+#*******************************************************   Session Setup *****************************************************************
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
@@ -18,7 +22,14 @@ for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
+#********************************************************** Sidebar UI *************************************************************************
 
+st.sidebar.title("LangGraph Chatbot")
+st.sidebar.button("New Chat")
+st.sidebar.header("Previous Chats")
+
+
+#********************************************************** Main UI *************************************************************************
 # Take user message
 user_input = st.chat_input('Type here')
 
